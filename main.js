5ed1371da81277d0afb7d8fb1da2a472d61cbed1
apeterlein <11824265+apeterlein@users.noreply.github.com>
2023-07-22 21:14:27 -0500

    G and Q handling

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 "use strict";
 
 $(document).ready(function () {
+	$('.text-area').highlightWithinTextarea({
+		highlight: [
+			{ highlight: /[g]+/g, className: 'hl-G' },
+			{ highlight: /[q]+/g, className: 'hl-Q' }
+		]
+	});
 	$("#file").change(function () {
 		readImage(document.getElementById("file").files[0]).then(function (out) {
 			$("#out").val(out.data.text.trim());
@@ -9,6 +15,10 @@ $(document).ready(function () {
 	});
 	$("#go").click(function () {
 		window.open($("#out").val().replace(/[\n\r]/g, ""), '_blank').focus();
+	});
+	$("#gq").click(function () {
+		$("#out").val($("#out").val().replace(/[g]/, "q"));
+		$('.text-area').highlightWithinTextarea('update');
 	});
 	document.addEventListener("dragover", function (evt) { evt.preventDefault(); } );
 	document.addEventListener("dragenter", function (evt) { evt.preventDefault(); } );
